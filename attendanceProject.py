aab5bfacb50504e6fee06acc58953c162a08466d
Ka3854 <103432574+Ka3854@users.noreply.github.com>
2022-05-26 16:00:18 +0530

    feature added to record attendance in csv

--- attendanceProject.py
+++ attendanceProject.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
-#from datetime import datetime
+from datetime import datetime
 # from PIL import ImageGrab
  
 path = 'C:/Users/Vartika/PycharmProjects/FaceRecognitionProject/ImagesAttendance'
@@ -25,11 +25,18 @@ def findEncodings(images):
        encodeList.append(encode)
     return encodeList
 # marking attendance function using csv
-# def markAttendance(name):
-#   with open('Attendance.csv','r+') as f:
-#   myDataList = f.readlines()
-#   print(myDataList)
-#nameList = []
+def markAttendance(name):
+   with open('Attendance.csv','r+') as f:
+       myDataList = f.readlines()
+       nameList = []
+       for line in myDataList:
+           entry = line.split(',')
+           nameList.append(entry[0])
+       if name not in nameList:
+           now = datetime.now()
+           dtString = now.strftime('%H:%M:%S')
+           f.writelines(f'\n{name},{dtString}')
+
 encodeListknown = findEncodings(images)
 print('Encoding complete')
 #capturing image on device camera
@@ -60,6 +67,7 @@ while True:
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
 
